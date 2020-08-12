34ae643599de03b8efcc3b2b58892a2a5120991a
wwb <2530920154@qq.com>
2020-08-12 10:57:35 +0800

    face:解决编程式导航多次点击的bug

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,25 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import routes from "@/router/routes";
 Vue.use(VueRouter);
-
+//保存之前的方法
+const originPush = VueRouter.prototype.push;
+const originReplace = VueRouter.prototype.replace;
+//修改原型上的方法
+VueRouter.prototype.push = function(config, OnResovled, OnRejected) {
+  //如果push没有传递解决方案，自定义函数调用
+  if (OnResovled === undefined && OnRejected === undefined) {
+    return originPush.call(this, config).catch(() => {});
+  } else {
+    return originPush.call(this, config, OnResovled, OnRejected);
+  }
+};
+VueRouter.prototype.replace = function(config, OnResovled, OnRejected) {
+  if (OnResovled === undefined && OnRejected === undefined) {
+    return originReplace.call(this, config).catch(() => {});
+  } else {
+    return originReplace.call(this, config, OnResovled, OnRejected);
+  }
+};
 export default new VueRouter({
   routes,
 });
